f8210912fab8c46481a935077ca5ba7a94bd6d83
MiXACT <out_lawz@mail.ru>
2022-07-26 15:20:41 +0300

    sub-menu open-close work out

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,23 @@
 const menuSub = Array.from(document.querySelectorAll('.menu__item ul'));
 const menuLink = Array.from(document.querySelectorAll('.menu__item a'));
-let oldIndex;
+let oldIndex, activeMenu;
 
 
 for (let i = 0; i < menuSub.length; i++) {
     menuLink.forEach((link) => {
         if (link.closest('.menu__item') == menuSub[i].closest('.menu__item')) {
             link.onclick = () => {
-                if (oldIndex != undefined) {
+                if (oldIndex != undefined && activeMenu != i) {
                     menuSub[oldIndex].classList.remove('menu_active');
+                    menuSub[i].classList.add('menu_active');
+                    activeMenu = i;
+                } else if (activeMenu == i) {
+                    menuSub[oldIndex].classList.remove('menu_active');
+                    activeMenu = null;
+                } else {
+                    menuSub[i].classList.add('menu_active');
+                    activeMenu = i;
                 };
-                menuSub[i].classList.add('menu_active');
                 oldIndex = i;
                 return false;
             };
