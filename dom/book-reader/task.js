68b10779adaf2593302359ec9e0a59e57785be6c
MiXACT <out_lawz@mail.ru>
2022-08-21 21:00:06 +0300

    book_r_active_circle

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,6 +1,6 @@
 const book = document.querySelector('.book');
 const fontCtrl = document.querySelector('.book__control');
-
+let prevFont = document.querySelector('.font-size_active');
 
 function fontResize(size) {
     if (size) book.setAttribute('class', `book book_fs-${size}`);
@@ -10,6 +10,9 @@ function fontResize(size) {
 
 fontCtrl.onclick = function(event) {
     let curFont = event.target;
+    prevFont.classList.remove('font-size_active');
+    curFont.classList.add('font-size_active');
+    prevFont = curFont;
     fontResize(curFont.getAttribute('data-size'));
     return false;
 }
